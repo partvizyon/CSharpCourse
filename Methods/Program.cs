c58b49de45b64893518fc610cdbfbbd26b3e99ea
partvizyon <elawhitestone@gmail.com>
2020-04-22 14:40:54 +0300

    Method Sample1 OK.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -13,7 +13,7 @@ namespace Methods
             Add();
             Add();
             Add();
-            var result = Add2(20,30);
+            var result = Add2(20,10);   // İkinci parametre olan 10 burada girilmezse 26. satırdaki 2. parametre DEFAULT olarak işlem görür!
             Console.WriteLine(result);
             Console.ReadLine();
             }
@@ -23,7 +23,7 @@ namespace Methods
             Console.WriteLine("Eklendi!");
             }
 
-        static int Add2(int number1, int number2)
+        static int Add2(int number1, int number2=70) // Defaılt değerler her zaman son parametrede olmalıdır! (Bu Örnekte son parametre number2)
             {
             var result = number1 + number2;
             return result;
